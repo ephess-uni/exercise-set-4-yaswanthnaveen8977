936e6a93b34ae48d4d710b9710af56adcd6786d5
yaswanthnaveen8977 <yvadalas@cbu.edu>
2024-02-25 03:23:35 -0600

    Update ex_4_3.py

--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -14,22 +14,15 @@ except ImportError:
 FILENAME = get_data_file_path("messages.log")
 # >>>> DO NOT MODIFY CODE ABOVE <<<<
 
+
 def time_between_shutdowns(logfile):
-    shutdown_events = get_shutdown_events(logfile)
+    """
+    Your docstring here.  Replace the pass keyword below with your implementation.
+    """
+    shutdowns = get_shutdown_events(logfile)
+    return logstamp_to_datetime(shutdowns[-1].split()[1]) - logstamp_to_datetime(shutdowns[0].split()[1])
 
-    if len(shutdown_events) < 2:
-        raise ValueError("There should be at least two shutdown events to calculate time difference.")
 
-    # Convert the date fields to datetime objects
-    first_shutdown_datetime = logstamp_to_datetime(shutdown_events[0])
-    last_shutdown_datetime = logstamp_to_datetime(shutdown_events[-1])
-
-    # Calculate the time difference
-    time_difference = last_shutdown_datetime - first_shutdown_datetime
-
-    return time_difference
-
-# Example usage:
-if __name__ == "__main__":
-    time_difference = time_between_shutdowns(FILENAME)
-    print(f"Time between first and last shutdowns: {time_difference}")
+# >>>> The code below will call your function and print the results
+if _name_ == "_main_":
+    print(f'{time_between_shutdowns(FILENAME)=}')
